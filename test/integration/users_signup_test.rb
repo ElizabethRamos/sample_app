f1325d2230d88f4a3a334c87e186e594a540a039
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-02-20 11:54:11 +0200

    Add success flash to the integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,6 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_select flash.success
     assert_not flash.alert
    end 
   end
